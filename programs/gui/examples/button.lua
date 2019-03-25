96dfcd3b9bbfc0d3cd1779d57086180c2d178525
Mark Hillman <markhillman94@googlemail.com>
2019-03-25 22:10:04 +0000

    Add more functionality to the button example

--- programs/gui/examples/button.lua
+++ programs/gui/examples/button.lua
@@ -1,27 +1,46 @@
 -- Create the application object
 local app = guiapplication.GUIApplication:new()
 
+local w, h = term.getSize()
+
 -- Create a root gui component. This is the base component used 
 -- in a similar way to JFrame with no decorations.
 local root = guirootcontainer.GUIRootContainer:new()
 
+-- Title text that should be centered
+local title = guitext.GUIText:new("Example Button Application")
+title:setAlign("center")
+title:setVerticalAlign("middle")
+title:setBorder(false)
+title:setPreferredBounds(2, 3, w - 2, 3)
+
+local clickLabel = guitext.GUIText:new("Click Count:")
+clickLabel:setAlign("right")
+clickLabel:setVerticalAlign("middle")
+clickLabel:setPreferredBounds(2, 8, (w - 2) / 2, 3)
+clickLabel:setBorder(false)
+
+local clickCount = guitext.GUIText:new("0")
+clickCount:setAlign("left")
+clickCount:setVerticalAlign("middle")
+clickCount:setPreferredBounds((w / 2) + 1, 8, (w - 2) / 2, 3)
+clickCount:setBorder(false)
+
 -- Create a new button with desired size and position.
 -- Position is relative to root component.
 local button = guibutton.GUIButton:new("Click me!")
-button:setPreferredBounds(3, 3, 15, 9)
+button:setPreferredBounds(3, 15, w - 2, 9)
+local count = 0
 local onClick = function(e)
-    -- print("Ya clicked the button ya goof")
-    -- print("x: "..e.x..", y: "..e.y)
-    -- app:stop()
+    count = count + 1
+    clickCount:setText(tostring(count))
 end
 button:addButtonListener(onClick)
 
-local text = guitext.GUIText:new("Hello World")
-text:setPreferredBounds(3, 15, 15, 1)
-text:setBorder(false)
-
 -- Add the button to the root container.
+root:add(title)
+root:add(clickLabel)
+root:add(clickCount)
 root:add(button)
-root:add(text)
 app:setRootContainer(root)
 app:start()
