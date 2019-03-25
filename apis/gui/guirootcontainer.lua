0aeb524441ced6caad333a0dcd979830ea205413
Mark Hillman <markhillman94@googlemail.com>
2019-03-25 08:32:29 +0000

    Remove print out from root component

--- apis/gui/guirootcontainer.lua
+++ apis/gui/guirootcontainer.lua
@@ -4,13 +4,6 @@ function GUIRootContainer:new(t)
     local object = guicomponent.GUIComponent:new(t)
     self.__index = self
     setmetatable(object, self)
-
-    -- testing mouse click 
-    local pe = function(e)
-        print("event: "..e.type)
-    end
-    object:addMouseClickListener(pe)
-
     return object
 end
 
