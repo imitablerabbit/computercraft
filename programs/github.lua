a18823216c0ebddda524f321bb4da83c4433a69a
Mark Hillman <markhillman94@googlemail.com>
2019-03-26 10:08:07 +0000

    Update github script to pull from a specific branch
    Closes #2

--- programs/github.lua
+++ programs/github.lua
@@ -2,6 +2,31 @@
 Pull all of the files from github using the http api.
 --]]
 
+-- Print a usage string on how to use the command
+function usage()
+    print("github [branch]")
+end
+
+-- Fetch a single raw file from github url
+function fetchFile(url, filepath)
+    if not url then error("missing url") end
+    if not filepath then error("missing file path") end
+
+    r = http.get(url)
+    if not r then return false end
+    if r.getResponseCode() ~= 200 then
+        r.close()
+        return false
+    end
+    d = r.readAll()
+    r.close()
+    h = fs.open(filepath, "w")
+    h.write(d)
+    h.close()
+    return true
+end
+
+-- Global variables
 local files = {
     "apis/files.lua",
     "apis/inventory.lua",
@@ -35,30 +60,26 @@ local files = {
 
     "startup.lua",
 }
+local branch = "master"
+local githubBaseURL = "https://raw.github.com/imitablerabbit/computercraft/"
 
-local githubURL = "https://raw.github.com/imitablerabbit/computercraft/master/"
-
--- Fetch a single raw file from github url
-function fetchFile(url, filepath)
-    if not url then error("missing url") end
-    if not filepath then error("missing file path") end
-
-    r = http.get(url)
-    if not r then return false end
-    if r.getResponseCode() ~= 200 then
-        r.close()
-        return false
-    end
-    d = r.readAll()
-    r.close()
-    h = fs.open(filepath, "w")
-    h.write(d)
-    h.close()
-    return true
+-- Parse command line args
+local args = {...}
+if #args > 1 then
+  usage()
+  return
+end
+if args[1] then
+  if args[1] == "help" or args[1] == "-help" or args[1] == "--help" then
+    usage()
+    return
+  end
+  branch = args[1]
 end
 
+githubURL = githubBaseURL..branch.."/"
 for k, file in pairs(files) do
-    url = githubURL .. file
+    url = githubURL..file
     print("fetching "..file)
     if not fetchFile(url, file) then
         print("Error: unable to fetch file: "..file)
