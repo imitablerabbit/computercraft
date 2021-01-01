55033c9ba184cb0672041e8576b3eba1d2f268ed
Mark Hillman <markhillman94@googlemail.com>
2021-01-01 17:26:45 +0000

    Add height input

--- programs/turtle/loggergui.lua
+++ programs/turtle/loggergui.lua
@@ -32,31 +32,44 @@ local widthInput = guiinput.GUIInput.new(loggerWidth)
 widthInput:setPreferredBounds(inputX, 3, inputW, inputH) 
 widthInput:setBorder(true)
 
+-- Setup the logging height label and input.
+local heightLabel = guitext.GUIText.new("Height:")
+heightLabel:setTextAlign("left")
+heightLabel:setTextVerticalAlign("middle")
+heightLabel:setPreferredBounds(labelX, 6, labelW, labelH)
+heightLabel:setBorder(false)
+local heightInput = guiinput.GUIInput.new(loggerHeight)
+heightInput:setPreferredBounds(inputX, 6, inputW, inputH) 
+heightInput:setBorder(true)
+
 -- Add button to change whether the contents of the turtle should be dumped in
 -- the chest placed behind it.
-local dumpItemsButton = guibutton.GUIButton.new("Dump Items")
-dumpItemsButton:setPreferredBounds(w - 1 - bw, 7, bw, 5)
-local dumpItemsClick = function(e)
-  dumpItems = not dumpItems
-  if dumpItems then
-    dumpItemsButton:setText("Dumping Items")
-  else
-    dumpItemsButton:setText("Not Dumping Items")
-  end
-end
-dumpItemsButton:addButtonListener(dumpItemsClick)
+--local dumpItemsButton = guibutton.GUIButton.new("Dump Items")
+--dumpItemsButton:setPreferredBounds(w - 1 - bw, 7, bw, 5)
+--local dumpItemsClick = function(e)
+--  dumpItems = not dumpItems
+--  if dumpItems then
+--    dumpItemsButton:setText("Dumping Items")
+--  else
+--    dumpItemsButton:setText("Not Dumping Items")
+--  end
+--end
+--dumpItemsButton:addButtonListener(dumpItemsClick)
 
 -- Start logging button. When clicked the turtle will start chooping down trees.
-local bw = math.floor((w - 4) / 2)
-local startButton = guibutton.GUIButton.new("Start Logging")
-startButton:setPreferredBounds(3, 7, bw, 5)
-local startClick = function(e)
-  shell.run("logger.lua", loggerWidth, loggerHeight, sleepTime, saplingName, tostring(dumpItems))
-end
-startButton:addButtonListener(addClick)
+--local bw = math.floor((w - 4) / 2)
+--local startButton = guibutton.GUIButton.new("Start Logging")
+--startButton:setPreferredBounds(3, 7, bw, 5)
+--local startClick = function(e)
+--  shell.run("logger.lua", loggerWidth, loggerHeight, sleepTime, saplingName, tostring(dumpItems))
+--end
+--startButton:addButtonListener(addClick)
 
 -- Add the button to the root container.
 root:add(widthLabel)
+root:add(widthInput)
+root:add(heightLabel)
+root:add(heightInput)
 --root:add(addClickLabel)
 --root:add(subtractClickLabel)
 --root:add(startButton)
